eb427c036a3e6193e751ca3b08232b0cf99521fd
Siddarthan  K <siddarthan.k@m2pfintech.com>
2024-04-03 16:15:09 +0530

    Single table controllers

--- src/main/java/com/example/DCMS/repository/UserRepository.java
+++ src/main/java/com/example/DCMS/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.example.DCMS.repository;
 
-import com.example.DCMS.model.BinReq;
 import com.example.DCMS.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
