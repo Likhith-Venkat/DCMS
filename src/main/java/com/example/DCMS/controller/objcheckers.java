cbd5ac040678904137e20227bb3d0d56aafce614
Siddarthan  K <siddarthan.k@m2pfintech.com>
2024-04-04 13:15:20 +0530

    obj checks

---
+++ src/main/java/com/example/DCMS/controller/objcheckers.java
@@ -0,0 +1,84 @@
+package com.example.DCMS.controller;
+
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class objcheckers {
+    @Autowired
+    MongoOperations mongoOperations;
+
+    public void ifChannelLimitAddToProduct(Document res) {
+        if (Objects.equals(res.getString("objectType"), "CHANNELLIMIT")) {
+            String productName = res.getString("productName");
+            Criteria cr = Criteria.where("objectType").is("PRODUCT").and("productName").is(productName);
+            Query qr = new Query(cr);
+            Document productDoc = mongoOperations.findOne(qr, Document.class, "mcobjects");
+            assert productDoc != null;
+            productDoc.put("channelLimit", res);
+            mongoOperations.save(productDoc, "mcobjects");
+        }
+
+    }
+
+    public void ifFeeConfigAddToProduct(Document res) {
+        if (Objects.equals(res.getString("objectType"), "FEECONFIG")) {
+            String productName = res.getString("productName");
+            Criteria cr = Criteria.where("objectType").is("PRODUCT").and("productName").is(productName);
+            Query qr = new Query(cr);
+            Document productDoc = mongoOperations.findOne(qr, Document.class, "mcobjects");
+            assert productDoc != null;
+            productDoc.put("feeConfig", res);
+            mongoOperations.save(productDoc, "mcobjects");
+        }
+
+    }
+
+    public Boolean isUnique(String objectType, String key, String value)
+    {
+
+        Criteria cr = Criteria.where("objectType").is(objectType).and(key).is(value);
+        Query qr = new Query(cr);
+        if(mongoOperations.findOne(qr, Document.class, "mcobjects")!= null) {
+            return false;
+        }
+        return true;
+    }
+    public Boolean exists(String objectType, String key, String value)
+    {
+        Criteria cr1 = Criteria.where("objectType").is(objectType).and(key).is(value);
+        Query qr1 = new Query(cr1);
+        if(mongoOperations.findOne(qr1, Document.class, "mcobjects")== null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean checkUniqueObjs(Document doc)
+    {
+        Boolean rtn = false;
+        switch(doc.getString("objectType")) {
+            case "BIN":
+                rtn = isUnique("BIN", "bin", doc.getString("bin"));
+            case "BINRANGE":
+                rtn = exists("BIN", "bin", doc.getString("bin")) && isUnique("BINRANGE", "binrangeName", doc.getString("binrangeName"));
+            case "PRODUCT":
+                rtn = isUnique("PRODUCT", "productName", doc.getString("productName"));
+                break;
+            case "CHANNELLIMIT":
+                rtn = exists("PRODUCT", "productName", doc.getString("productName"));
+                break;
+            case "FEECONFIG":
+                rtn = exists("PRODUCT", "productName", doc.getString("productName"));
+                break;
+        }
+        return rtn;
+    }
+}
--- src/main/java/com/example/DCMS/controller/objectsController.java
+++ src/main/java/com/example/DCMS/controller/objectsController.java
@@ -1,11 +1,7 @@
 package com.example.DCMS.controller;
 
 import com.example.DCMS.config.MyUserDetails;
-import com.example.DCMS.model.*;
-import com.example.DCMS.repository.*;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import org.bson.types.ObjectId;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -20,12 +16,10 @@ import org.springframework.web.bind.annotation.*;
 import org.bson.Document;
 import org.springframework.web.server.ResponseStatusException;
 
-
-import javax.print.Doc;
 import java.util.*;
 
 @RestController
-@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasRole('ADMIN') or hasRole('SUPPORT')")
 @RequestMapping("/mc")
 public class objectsController
 {
@@ -33,11 +27,13 @@ public class objectsController
 
     @Autowired
     MongoOperations mongoOperations;
+    @Autowired
+    objcheckers objchks;
 
 
 
         @PreAuthorize("hasRole('CHECKER')")
-    @PostMapping(path = "/approveobj")
+    @PutMapping(path = "/approveobj")
     public ResponseEntity<Document> approveobj(Authentication auth, @RequestBody String req)
     {
         MyUserDetails userDetails = (MyUserDetails) auth.getPrincipal();
@@ -48,6 +44,7 @@ public class objectsController
             Criteria cr = Criteria.where("_id").is(id);
             Query qr = new Query(cr);
             Document doc = mongoOperations.findOne(qr, Document.class, "mcobjects");
+            assert doc != null;
             if(!Objects.equals(doc.getString("status"), "PENDING"))
             {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Object Alreay checked");
@@ -55,6 +52,8 @@ public class objectsController
             doc.replace("status", "ACCEPTED");
             doc.put("checker", userDetails.getUsername());
             Document res = mongoOperations.save(doc, "mcobjects");
+            objchks.ifChannelLimitAddToProduct(res);
+            objchks.ifFeeConfigAddToProduct(res);
             return new ResponseEntity<>(res, HttpStatus.CREATED);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request body format");
@@ -62,12 +61,13 @@ public class objectsController
 
     }
     @PreAuthorize("hasRole('CHECKER')")
-    @PostMapping(path = "/rejectobj")
+    @PutMapping(path = "/rejectobj")
     public ResponseEntity<?> rejectobj(Authentication auth,  @RequestBody String req)
     {
         MyUserDetails userDetails = (MyUserDetails) auth.getPrincipal();
 
         try {
+
             JSONObject jsonReq = new JSONObject(req);
             String id =jsonReq.getString("id");
             String rejectReason = jsonReq.getString("rejectReason");
@@ -83,6 +83,7 @@ public class objectsController
             doc.replace("status", "REJECTED");
             doc.put("checker", userDetails.getUsername());
             Document res = mongoOperations.save(doc, "mcobjects");
+
             return new ResponseEntity<>(res, HttpStatus.CREATED);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request body format");
@@ -103,11 +104,16 @@ public class objectsController
         MyUserDetails userDetails = (MyUserDetails) auth.getPrincipal();
 
         try {
+
             JSONObject jsonReq = new JSONObject(req);
             jsonReq.put("status", "PENDING");
             jsonReq.put("maker", userDetails.getUsername());
             Document document = Document.parse(jsonReq.toString());
             document.put("_id", UUID.randomUUID().toString());
+            if(!objchks.checkUniqueObjs(document))
+            {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Object already exists/wrong attributes");
+            }
             Document res = mongoOperations.save(document, "mcobjects");
             return new ResponseEntity<>(res, HttpStatus.CREATED);
         } catch (Exception e) {
