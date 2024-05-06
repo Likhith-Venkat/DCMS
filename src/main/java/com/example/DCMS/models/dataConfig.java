1f950234b36239d4c2d043d5de2a342ab82d5ce0
Likhith Venkat Pagadala <likhithvenkat.pagadala@m2pfintech.com>
2024-05-06 13:07:31 +0530

    mongo audit config

---
+++ src/main/java/com/example/DCMS/models/dataConfig.java
@@ -0,0 +1,9 @@
+package com.example.DCMS.models;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class dataConfig {
+    // Additional configurations if needed
+}
